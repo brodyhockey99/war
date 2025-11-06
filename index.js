be31a1e17faff370ebc2721d02dff1eec06b939c
ODD <108240139+brodyhockey99@users.noreply.github.com>
2025-11-05 19:33:32 -0500

    Create index.js

---
+++ index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import './index.css'; // optional, can create for styles
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<App />);
